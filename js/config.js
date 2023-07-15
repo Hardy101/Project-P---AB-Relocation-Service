7b923d0334c1a6260e8f024d2a0a8705aeaa3af5
Eghosa Ordia <eghordia130@gmail.com>
2023-07-15 16:37:40 +0100

    first commit

---
+++ js/config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        primaryRed: "#d10a11",
+        primaryBlack: "#rgb(7, 7, 7)",
+      },
+    },
+  },
+};
